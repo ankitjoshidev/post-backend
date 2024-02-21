2cc983eb69419fb34a935d4816f7e5223bd59649
Ankit Joshi <ankitjoshi66259@gmail.com>
2024-02-21 19:03:01 +0530

    added vercel config

--- config/database.js
+++ config/database.js
@@ -1,14 +1,22 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(
-  'post', // Replace with your database name
-  'postgres', // Replace with your database username
-  '1234', // Replace with your database password
-  {
-    host: 'localhost',
-    dialect: 'postgres',
-    port: 5432, // Default PostgreSQL port
-  }
-);
+// Postgres database config
+// const sequelize = new Sequelize(
+//   'post', // Replace with your database name
+//   'postgres', // Replace with your database username
+//   '1234', // Replace with your database password
+//   {
+//     host: 'localhost',
+//     dialect: 'postgres',
+//     port: 5432, // Default PostgreSQL port
+//   }
+// );
+
+
+// Sqlite database configs
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'database.sqlite',
+});
 
 module.exports = sequelize;
